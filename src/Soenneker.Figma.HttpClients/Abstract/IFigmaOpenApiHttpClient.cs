236f1f5df692230bbcd3743ef45c0731b67f0dc0
soenneker <jake@soenneker.com>
2026-08-29 14:17:03 -0500

    Improve public API XML documentation

--- src/Soenneker.Figma.HttpClients/Abstract/IFigmaOpenApiHttpClient.cs
+++ src/Soenneker.Figma.HttpClients/Abstract/IFigmaOpenApiHttpClient.cs
@@ -2,13 +2,16 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Threading;
-
 namespace Soenneker.Figma.HttpClients.Abstract;
-
 /// <summary>
 /// A .NET thread-safe singleton HttpClient for 
 /// </summary>
 public interface IFigmaOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured HTTP client used by the Figma OpenAPI HTTP Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested HTTP client.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Figma.HttpClients/Registrars/FigmaOpenApiHttpClientRegistrar.cs
+++ src/Soenneker.Figma.HttpClients/Registrars/FigmaOpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class FigmaOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="FigmaOpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddFigmaOpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class FigmaOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="FigmaOpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddFigmaOpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
